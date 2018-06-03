3095e60fa7fa3588e25053ac6ea7165847faeb0e
Maxim Penderuk <maxim.penderuk@gmail.com>
2018-06-03 21:06:28 +0300

    Styles fixes

--- src/containers/add_financial_record_form.js
+++ src/containers/add_financial_record_form.js
@@ -1,14 +1,17 @@
-import React, { Component }   from 'react';
+import React, { Component, Fragment } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect }            from 'react-redux';
 import Select                 from 'react-select';
 import PropTypes              from 'prop-types';
+import LIVR                   from 'livr';
+import SweetAlert             from 'sweetalert-react';
 import moment                 from 'moment';
 
 import { addFinancialRecord } from '../actions/index';
 
 import 'react-select/dist/react-select.css';
 import './add_financial_record_form.css';
+import 'sweetalert/dist/sweetalert.css';
 
 const SELECT_OPTIONS = {
   types: [
@@ -33,7 +36,9 @@ export class AddFinancialRecordFrom extends Component {
       amount          : '100.00',
       description     : '',
       selectedType    : SELECT_OPTIONS.types[0],
-      selectedCurrency: SELECT_OPTIONS.currencies[0]
+      selectedCurrency: SELECT_OPTIONS.currencies[0],
+      errorString     : '',
+      errors          : {}
     };
   }
 
@@ -46,18 +51,29 @@ export class AddFinancialRecordFrom extends Component {
     }
   }
 
-  handleAmountChange = event => {
-    this.setState({ amount: event.target.value });
-  }
+  handleInputChange = (input, event) => {
+    const state = { ...this.state };
 
-  handleDescriptionChange = event => {
-    this.setState({ description: event.target.value });
+    state[input] = event.target.value;
+    delete state.errors[input];
+
+    this.setState(state);
   }
 
   handleFormSubmit = event => {
     event.preventDefault();
 
     const { selectedType, selectedCurrency, description, amount } = this.state;
+    const isValidData = this.validate({
+      type       : selectedType.value,
+      currency   : selectedCurrency.value,
+      createdAt  : moment().unix(),
+      description,
+      amount
+    });
+
+    if (!isValidData) return;
+
     const financialRecord = {
       type       : selectedType.value,
       currency   : selectedCurrency.value,
@@ -73,56 +89,114 @@ export class AddFinancialRecordFrom extends Component {
       financialRecord.amounts.eur = parseFloat(Math.abs(amount));
     }
 
-    this.props.addFinancialRecord(financialRecord);
+    this.setState({
+      amount          : '100.00',
+      description     : ''
+    }, () => this.props.addFinancialRecord(financialRecord));
+  }
+
+  validate(financialRecord) {
+    const rules = {
+      type       : ['required', 'string', { 'one_of': ['expense', 'income'] } ],
+      currency   : ['required', 'string', { 'one_of': ['usd', 'eur'] }],
+      createdAt  : ['required', 'integer'],
+      description: ['trim'],
+      amount     : ['required', 'decimal']
+    };
+
+    const validator = new LIVR.Validator(rules);
+    const validatedResult = validator.validate(financialRecord);
+
+    if (!validatedResult) {
+      const errorCodes = {
+        REQUIRED   : 'required',
+        NOT_DECIMAL: 'not a number',
+        TOO_LONG   : 'required'
+      };
+      const errors = validator.getErrors();
+      let errorString = '';
+
+      for (const key in errors) {
+        const errorCode = errors[key];
+        const field = `${key.charAt(0).toUpperCase()}${key.slice(1)}`;
+        
+        errorString += `${field} is ${errorCodes[errorCode]}.\n`;
+      }
+      
+      this.setState({ errorString, errors });
+    }
+
+    return validatedResult;
+  }
+
+  handleAlertClick = () => {
+    this.setState({
+      errorString: ''
+    });
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
-      <form className='form-row' onSubmit={this.handleFormSubmit}>
-        <div className='form-group col-md-2'>
-          <Select
-            className='finance-type-select'
-            name='finance-type'
-            value={this.state.selectedType}
-            onChange={this.handleSelectOptionChange.bind(null, 'type')}
-            options={SELECT_OPTIONS.types}
-            clearable={false}
-            searchable={false}
-          />
-        </div>
-        <div className='form-group col-md-1'>
-          <Select
-            className='currency-select'
-            name='currency'
-            value={this.state.selectedCurrency}
-            onChange={this.handleSelectOptionChange.bind(null, 'currency')}
-            options={SELECT_OPTIONS.currencies}
-            clearable={false}
-            searchable={false}
-          />
-        </div>
-        <div className='form-group col-md-2'>
-          <input
-            type='text'
-            className='form-control amount'
-            placeholder='Amount'
-            value={this.state.amount}
-            onChange={this.handleAmountChange}
-          />
-        </div>
-        <div className='form-group col-md-5'>
-          <input
-            type='text'
-            className='form-control'
-            placeholder='Description'
-            value={this.state.description}
-            onChange={this.handleDescriptionChange}
-          />
-        </div>
-        <div className='form-group col-md-2'>
-          <button type='submit' className='btn btn-secondary'>Add</button>
-        </div>
-      </form>
+      <Fragment>
+        <form className='form-row financial-record-form' onSubmit={this.handleFormSubmit}>
+          <div className='form-group col-md-2'>
+            <Select
+              className='finance-type-select'
+              name='finance-type'
+              value={this.state.selectedType}
+              onChange={this.handleSelectOptionChange.bind(null, 'type')}
+              options={SELECT_OPTIONS.types}
+              clearable={false}
+              searchable={false}
+            />
+          </div>
+          <div className='form-group col-md-1'>
+            <Select
+              className='currency-select'
+              name='currency'
+              value={this.state.selectedCurrency}
+              onChange={this.handleSelectOptionChange.bind(null, 'currency')}
+              options={SELECT_OPTIONS.currencies}
+              clearable={false}
+              searchable={false}
+            />
+          </div>
+          <div className='form-group col-md-2'>
+            <input
+              type='text'
+              className={`form-control amount ${
+                errors.amount
+                ? 'is-invalid'
+                : ''
+              }`}
+              placeholder='Amount'
+              value={this.state.amount}
+              onChange={this.handleInputChange.bind(null, 'amount')}
+            />
+          </div>
+          <div className='form-group col-md-5'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Description'
+              value={this.state.description}
+              onChange={this.handleInputChange.bind(null, 'description')}
+            />
+          </div>
+          <div className='form-group col-md-2'>
+            <button type='submit' className='btn btn-secondary'>Add</button>
+          </div>
+        </form>
+        <SweetAlert
+          show={this.state.errorString ? true : false}
+          title='Oops!'
+          type='error'
+          text={this.state.errorString}
+          onConfirm={this.handleAlertClick}
+        />
+      </Fragment>
     );
   }
 }
--- src/containers/switch_currency_row.js
+++ src/containers/switch_currency_row.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import { bindActionCreators }         from 'redux';
-import { connect }                    from 'react-redux';
+import React, { Component, Fragment }   from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
 import Select                 from 'react-select';
-import PropTypes                      from 'prop-types';
-import debounce from 'lodash/debounce';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import PropTypes              from 'prop-types';
+import LIVR                   from 'livr';
+import SweetAlert             from 'sweetalert-react';
+import debounce               from 'lodash/debounce';
+import FontAwesomeIcon        from '@fortawesome/react-fontawesome';
 
-import { setConversionData } from '../actions/index';
+import { setConversionData }  from '../actions/index';
 
 import faArrow from '@fortawesome/fontawesome-free-solid/faArrowRight';
 
 import 'react-select/dist/react-select.css';
+import 'sweetalert/dist/sweetalert.css';
 import './switch_currency_row.css';
 
 const SELECT_OPTIONS = [
@@ -27,7 +30,11 @@ export class SwitchCurrencyRow extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { rate: props.conversionData.rate };
+    this.state = {
+      rate       : props.conversionData.rate,
+      errors     : {},
+      errorString: ''
+    };
   }
 
   handleClick = () => {
@@ -42,6 +49,10 @@ export class SwitchCurrencyRow extends Component {
   }
 
   setConversionData = debounce(() => {
+    const isValid = this.validate(this.state.rate);
+
+    if (!isValid) return;
+
     this.props.setConversionData({
       ...this.props.conversionData,
       rate: this.state.rate
@@ -50,57 +61,106 @@ export class SwitchCurrencyRow extends Component {
 
   handleRateChange = event => {
     this.setState({
-      rate: event.target.value
+      rate: event.target.value,
+      errors: {},
+      errorString: ''
     }, this.setConversionData);
   }
 
   renderTest = currency => {
     if (currency === 'usd') {
       return (
-        <label className='mr-sm-2 ml-sm-2'>
+        <label className='mr-sm-2 ml-sm-2 mr-2 ml-2'>
           € <FontAwesomeIcon icon={faArrow} /> $
         </label>
       );
     } else {
       return (
-        <label className='mr-sm-2 ml-sm-2'>
-          $ <FontAwesomeIcon icon={faArrow} /> €
+        <label className='mr-sm-2 ml-sm-2 mr-2 ml-2'>
+          $ <FontAwesomeIcon icon={faArrow} />  €
         </label>
       );
     }
   }
 
+  handleAlertClick = () => {
+    this.setState({
+      errorString: ''
+    });
+  }
+
+  validate(currency) {
+    const rules = {
+      currency: ['required', 'decimal']
+    };
+    const validator = new LIVR.Validator(rules);
+    const validatedResult = validator.validate({ currency });
+
+    if (!validatedResult) {
+      const errorCodes = {
+        REQUIRED   : 'required',
+        NOT_DECIMAL: 'not a number'
+      };
+      const errors = validator.getErrors();
+      let errorString = '';
+
+      for (const key in errors) {
+        const errorCode = errors[key];
+        const field = `${key.charAt(0).toUpperCase()}${key.slice(1)}`;
+        
+        errorString += `${field} is ${errorCodes[errorCode]}.\n`;
+      }
+      
+      this.setState({ errorString, errors });
+    }
+
+    return validatedResult;
+  }
 
   render() {
     const { currency } = this.props.conversionData;
+    const { errors } = this.state;
     const selectedOption = SELECT_OPTIONS.find(item => item.value === currency);
     const renderLabel = this.renderTest.bind(null, currency);
 
     return (
-      <form className='form-inline currency-switch-form'>
-        <label
-          htmlFor='currency-select'
-          className='currency-select-label mr-sm-2'
-        >
-          Display amount in
-        </label>
-        <Select
-          className='currency-select'
-          name='currency'
-          value={selectedOption}
-          options={SELECT_OPTIONS}
-          onChange={this.handleCurrencyChange}
-          clearable={false}
-          searchable={false}
+      <Fragment>
+        <form className='form-inline currency-switch-form'>
+          <label
+            htmlFor='currency-select'
+            className='currency-select-label mr-sm-2 mr-2'
+          >
+            Display amount in
+          </label>
+          <Select
+            className='currency-select'
+            name='currency'
+            value={selectedOption}
+            options={SELECT_OPTIONS}
+            onChange={this.handleCurrencyChange}
+            clearable={false}
+            searchable={false}
+          />
+          { renderLabel() }
+          <input
+            type='text'
+            className={`form-control ${
+              errors.currency
+              ? 'is-invalid'
+              : ''
+            }`}
+            value={this.state.rate}
+            onChange={this.handleRateChange}
+          />
+        </form>
+        <SweetAlert
+          show={this.state.errorString ? true : false}
+          title='Oops!'
+          type='error'
+          text={this.state.errorString}
+          onConfirm={this.handleAlertClick}
         />
-        { renderLabel() }
-        <input
-          type='text'
-          className='form-control'
-          value={this.state.rate}
-          onChange={this.handleRateChange}
-        />
-      </form>
+      </Fragment>
     );
   }
 }

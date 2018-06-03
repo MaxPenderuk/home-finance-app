682aadadccd952d168be709d4d0635fb2ad87936
Maxim Penderuk <maxim.penderuk@gmail.com>
2018-06-03 22:16:17 +0300

    Initial commit

--- src/etc/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyAVrYYlixD5zjN3CtbdqZCBYlbBs11gqaA",
-  authDomain: "home-finance-app-eefc1.firebaseapp.com",
-  databaseURL: "https://home-finance-app-eefc1.firebaseio.com",
-  projectId: "home-finance-app-eefc1",
-  storageBucket: "home-finance-app-eefc1.appspot.com",
-  messagingSenderId: "642441633626"
-};
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-import financialRecords from './reducer_financial_records';
-
-export default combineReducers({ financialRecords });

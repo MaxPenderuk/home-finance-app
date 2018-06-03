2197c61055fc260ed0531e69b5d8d1801cd7a333
Maxim Penderuk <maxim.penderuk@gmail.com>
2018-06-03 21:58:08 +0300

    Code indent

--- src/containers/financial_records_list/index.js
+++ src/containers/financial_records_list/index.js
@@ -18,10 +18,10 @@ import './style.css';
 
 export class FinancialRecordsList extends Component {
   static propTypes = {
-    children             : PropTypes.node.isRequired,
-    financialRecords     : PropTypes.object.isRequired,
-    loadFinancialRecords : PropTypes.func.isRequired,
-    removeFinancialRecord: PropTypes.func.isRequired
+    children              : PropTypes.node.isRequired,
+    financialRecords      : PropTypes.object.isRequired,
+    loadFinancialRecords  : PropTypes.func.isRequired,
+    removeFinancialRecord : PropTypes.func.isRequired
   };
 
   componentDidMount() {
@@ -40,20 +40,20 @@ export class FinancialRecordsList extends Component {
         <span>
           {
             currency === 'usd'
-            ? `$ ${Number(item.amounts.usd).toFixed(2)}`
-            : `€ ${Number(item.amounts.eur).toFixed(2)}`
+              ? `$ ${Number(item.amounts.usd).toFixed(2)}`
+              : `€ ${Number(item.amounts.eur).toFixed(2)}`
           }
         </span>
         <span>
           {
             currency === 'usd' && item.amounts.eur
-            ? `€ ${Number(item.amounts.eur).toFixed(2)}`
-            : null
+              ? `€ ${Number(item.amounts.eur).toFixed(2)}`
+              : null
           }
           {
             currency === 'eur' && item.amounts.usd
-            ? `$ ${Number(item.amounts.usd).toFixed(2)}`
-            : null
+              ? `$ ${Number(item.amounts.usd).toFixed(2)}`
+              : null
           }
         </span>
       </div>
@@ -67,64 +67,65 @@ export class FinancialRecordsList extends Component {
 
     return (
       <Fragment>
-      { this.props.children }
-      <table className='table table-hover mt-3'>
-        <tbody>
-          {
-            list.map(item => {
-              const getAmountsBlock = this.renderAmounts.bind(null, item);
-              balance = item.type === 'expense'
-                ? balance - parseFloat(item.amounts[currency])
-                : balance + parseFloat(item.amounts[currency]);
+        { this.props.children }
+        <table className='table table-hover mt-3'>
+          <tbody>
+            {
+              list.map(item => {
+                const getAmountsBlock = this.renderAmounts.bind(null, item);
 
-              return (
-                <tr key={item.id}>
-                  <td style={{ textAlign: 'center' }}>
-                    {
-                      item.type === 'expense'
-                      ? <div className='arrow-down' />
-                      : <div className='arrow-up' />
-                    }
-                  </td>
-                  <td>
-                    <div className='description'>
-                      <span>{ item.description }</span>
-                      <span>{ moment.unix(item.createdAt).format('MMM DD, YYYY hh:mm a') }</span>
-                    </div>
-                  </td>
-                  <td>
-                    {
-                      item.type === 'expense'
-                      ? getAmountsBlock()
-                      : <div className='no-amounts'>{'--'}</div>
-                    }
-                  </td>
-                  <td>
-                    {
-                      item.type === 'income'
-                      ? getAmountsBlock()
-                      : <div className='no-amounts'>{'--'}</div>
-                    }
-                  </td>
-                  <td>
-                    <FontAwesomeIcon
-                      icon={faTimes}
-                      onClick={this.handleRecordDeleteClick.bind(null, item.id)}
-                    />
-                  </td>
-                </tr>
-              )
-            })
-          }
-          <tr>
-            <td></td>
-            <td>Balance</td>
-            <td>{ Math.sign(balance) < 0 ? `${currencySign[currency]} ${Math.abs(balance).toFixed(2)}` : '' }</td>
-            <td>{ Math.sign(balance) >= 0 ? `${currencySign[currency]} ${balance.toFixed(2)}` : '' }</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+                balance = item.type === 'expense'
+                  ? balance - parseFloat(item.amounts[currency])
+                  : balance + parseFloat(item.amounts[currency]);
+
+                return (
+                  <tr key={item.id}>
+                    <td style={{ textAlign: 'center' }}>
+                      {
+                        item.type === 'expense'
+                          ? <div className='arrow-down' />
+                          : <div className='arrow-up' />
+                      }
+                    </td>
+                    <td>
+                      <div className='description'>
+                        <span>{ item.description }</span>
+                        <span>{ moment.unix(item.createdAt).format('MMM DD, YYYY hh:mm a') }</span>
+                      </div>
+                    </td>
+                    <td>
+                      {
+                        item.type === 'expense'
+                          ? getAmountsBlock()
+                          : <div className='no-amounts'>{'--'}</div>
+                      }
+                    </td>
+                    <td>
+                      {
+                        item.type === 'income'
+                          ? getAmountsBlock()
+                          : <div className='no-amounts'>{'--'}</div>
+                      }
+                    </td>
+                    <td>
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        onClick={this.handleRecordDeleteClick.bind(null, item.id)}
+                      />
+                    </td>
+                  </tr>
+                )
+              })
+            }
+            <tr>
+              <td></td>
+              <td>Balance</td>
+              <td>{ Math.sign(balance) < 0 ? `${currencySign[currency]} ${Math.abs(balance).toFixed(2)}` : '' }</td>
+              <td>{ Math.sign(balance) >= 0 ? `${currencySign[currency]} ${balance.toFixed(2)}` : '' }</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
       </Fragment>
     );
   }
@@ -142,8 +143,13 @@ export class FinancialRecordsList extends Component {
       <Fragment>
         {
           isLoading
-          ? <div className='spinner-wrap'><div className='spinner'/></div>
-          : list.length ? this.renderDataTable(list) : this.renderNoRecordsBlock()
+            ? <div className='spinner-wrap'><div className='spinner'/></div>
+            : null
+        }
+        {
+          list.length
+            ? this.renderDataTable(list)
+            : this.renderNoRecordsBlock()
         }
       </Fragment>
     );

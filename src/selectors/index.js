ee2facc9f4a2842cc48ca6f7dc13a7d6cd8968a5
Maxim Penderuk <maxim.penderuk@gmail.com>
2018-06-04 15:37:39 +0300

    Fixed selector

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,28 +1,25 @@
 import { createSelector } from 'reselect';
 
-const getFinancialRecordsList = state => {
-  const newState = { ...state };
-  const { conversionData } = newState;
+const financialRecordsListSelector = state => state.list;
+const conversionDataSelector = state => state.conversionData;
 
-  return newState.list.map(item => {
-    if (conversionData.currency === 'usd' && item.currency === 'eur') {
-      item.amounts.usd = Math.round((item.amounts.eur * conversionData.rate) * 1000) / 1000;
-    }
-    if (conversionData.currency === 'usd' && item.currency === 'usd') {
-      delete item.amounts.eur;
-    }
-    if (conversionData.currency === 'eur' && item.currency === 'eur') {
-      delete item.amounts.usd;
-    }
-    if (conversionData.currency === 'eur' && item.currency === 'usd') {
-      item.amounts.eur = Math.round((item.amounts.usd * conversionData.rate) * 1000) / 1000;
-    }
-
-    return item;
-  });
-};
 
 export const getFinancialRecordsState = createSelector(
-  [ getFinancialRecordsList ],
-  list => list
+  [ financialRecordsListSelector, conversionDataSelector ],
+  (list, conversionData) => list.map(item => {
+    let usd = item.amounts.usd || null;
+    let eur = item.amounts.eur || null;
+
+    if (conversionData.currency === 'usd' && item.currency === 'eur') {
+      usd = Math.round((item.amounts.eur * conversionData.rate) * 1000) / 1000;
+    }
+    if (conversionData.currency === 'eur' && item.currency === 'usd') {
+      eur = Math.round((item.amounts.usd * conversionData.rate) * 1000) / 1000;
+    }
+
+    return {
+      ...item,
+      amounts: { ...item.amounts, eur, usd }
+    };
+  })
 );
